ae2f6eb17138684e5c77a9952f45664b29895db6
ultimate-knight <maaz2607@gmail.com>
2026-01-04 08:39:49 +0530

    fixed some errors in files

--- src/app/Add-Habit/page.js
+++ src/app/Add-Habit/page.js
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic'
 "use client";
 import { useSearchParams, useRouter } from "next/navigation";
 import { useState } from "react";
--- src/app/lib/messageUtils.js
+++
@@ -1,8 +0,0 @@
-export const replaceTemplateVars = (template, userData) => {
-  let msg = template;
-  Object.keys(userData).forEach((key) => {
-    const regex = new RegExp(`{{${key}}}`, "g");
-    msg = msg.replace(regex, userData[key] || "");
-  });
-  return msg;
-};
